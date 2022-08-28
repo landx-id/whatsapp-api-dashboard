a0e91f027cfc603cbae53fcd918596113e0f4e50
rafly <raflylesmana111@gmail.com>
2022-08-28 18:28:30 +0700

    initialize project

---
+++ index.js
@@ -0,0 +1,14 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
